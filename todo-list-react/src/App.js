3e389e2276d3840a515385d50fd585f4eb53404b
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-05-18 10:12:12 -0700

    build the basic todo list in react

--- todo-list-react/src/App.js
+++ todo-list-react/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          React TODO List
+        </h1>
       </header>
+      <main className="App-main">
+        <TodoList items={items} />
+        <AddTodo addItem={(newItem) => {
+          setItems(prev => {
+            return [...prev, newItem];
+          });
+        }} />
+      </main>
     </div>
+  );
+}
+
+function TodoList({items}) {
+  return (
+    <ol>
+      {items.map(item => {
+        return <TodoItem item={item} />;
+      })}
+    </ol>
+  );
+}
+
+function TodoItem({item}) {
+  return <li>{item.done ? <strike>{item.name}</strike> : item.name}</li>
+}
+
+function AddTodo({addItem}) {
+  return (
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      const formData = new FormData(event.target);
+      addItem({
+        name: formData.get('todo'),
+        done: false,
+      });
+      event.target.reset();
+    }}>
+      <input type="text" name="todo" autoFocus />
+      <button type="submit">Create</button>
+    </form>
   );
 }
 
